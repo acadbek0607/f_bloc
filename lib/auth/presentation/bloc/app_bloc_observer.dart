e7745fd1b24a08889e9ad468455124161f397725
vecious <acadbek0607@gmail.com>
2025-06-13 15:23:28 +0500

    edit

--- lib/auth/presentation/bloc/app_bloc_observer.dart
+++ lib/auth/presentation/bloc/app_bloc_observer.dart
@@ -6,4 +6,25 @@ class AppBlockObserver extends BlocObserver {
     super.onCreate(bloc);
     print('${bloc.runtimeType} Created! ${bloc.state}');
   }
+
+  oncCange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print(
+      '${bloc.runtimeType} Changed! ${change.currentState} -> ${change.nextState}',
+    );
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(
+      '${bloc.runtimeType} Transitioned! ${transition.currentState} -> ${transition.nextState}',
+    );
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    print('${bloc.runtimeType} Error! $error');
+  }
 }
